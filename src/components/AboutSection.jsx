76b926f3f7e819fa402431b32c5b0b929e9bc01a
saimzaidi110 <saimzaidi110786@gmail.com>
2025-09-12 13:03:58 +0500

    ---

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,100 +15,179 @@ const AboutSection = () => {
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Campus Highlights</h3>
-                <p className="about-text">
-                    Our campus is designed to inspire learning and collaboration, featuring advanced laboratories, smart classrooms, a state-of-the-art auditorium, and sports facilities. With a strong focus on holistic education, XYZ College has earned recognition for its active student clubs, entrepreneurial initiatives, and strong alumni network.
-                </p>
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+                <div className="content">
+                    <h3 className="about-heading">Campus Highlights</h3>
+                    <p className="about-text">
+                        Our campus is designed to inspire learning and collaboration, featuring advanced laboratories, smart classrooms, a state-of-the-art auditorium, and sports facilities. With a strong focus on holistic education, XYZ College has earned recognition for its active student clubs, entrepreneurial initiatives, and strong alumni network.
+                    </p>
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Technical (2023–24)</h3>
-                <p className="about-text">
-                    This section showcases the latest innovations, projects, and competitions in the technical field during the academic year 2023–24. It highlights students’ creativity and problem-solving abilities across domains such as robotics, coding, and tech design.
-                </p>
+                <div className='content'>
+                    <h3 className="about-heading">Hackathon (2023–24)</h3>
+                    <p className="about-text">
+                        The Hackathon was an intensive coding marathon where students collaborated to build innovative solutions within a limited time. Participants formed teams to brainstorm ideas, design prototypes, and present working projects that addressed real-world challenges. From web and mobile applications to AI-driven tools, the event showcased creativity, teamwork, and problem-solving at its best.
+                    </p>
+                </div>
+
+                <div className="banner">
+                    <img src="./galleryImages/gallery3.jpg" alt="" />
+                </div>
 
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Cultural Night 2022 <br />
-                    Year: 2022–23 | Category: Cultural</h3>
-                <p className="about-text">
-                    Cultural Night 2022 was a celebration of diversity, talent, and heritage. From traditional dances to contemporary performances, the event brought students together to showcase the rich tapestry of cultures and creativity that define campus life.
-                </p>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+
+                <div className='content'>
+                    <h3 className="about-heading">Cultural Night 2022 <br />
+                        Year: 2022–23 | Category: Cultural</h3>
+                    <p className="about-text">
+                        Cultural Night 2022 was a celebration of diversity, talent, and heritage. From traditional dances to contemporary performances, the event brought students together to showcase the rich tapestry of cultures and creativity that define campus life.
+                    </p>
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">GreenStep 2023 <br />
+                <div className="content">
+                    <h3 className="about-heading">GreenStep 2023 <br />
 
-                    Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                   GreenStep 2023 promoted wellness through movement. This sports-themed event focused on sustainability and health, encouraging participants to take a “green step” towards fitness while enjoying team-building and outdoor activities.
-                </p>
+                        Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        GreenStep 2023 promoted wellness through movement. This sports-themed event focused on sustainability and health, encouraging participants to take a “green step” towards fitness while enjoying team-building and outdoor activities.
+                    </p>
+                </div>
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Coderush 2023 <br />Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                   Coderush 2023 was a high-intensity programming challenge where coders raced against time to crack algorithms and build solutions. With its competitive spirit, it fostered innovation, logical thinking, and technical collaboration.
-                </p>
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+
+                <div className="content">
+                    <h3 className="about-heading">Coderush 2023 <br />Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        Coderush 2023 was a high-intensity programming challenge where coders raced against time to crack algorithms and build solutions. With its competitive spirit, it fostered innovation, logical thinking, and technical collaboration.
+                    </p>
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Finance Frontier 2024 <br />
-                    Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                   This event brought financial literacy to the forefront with simulations, mock stock trading, and entrepreneurial challenges. Finance Frontier 2024 was designed to inspire budding economists and business leaders of tomorrow.
-                </p>
+                <div className="content">
+                    <h3 className="about-heading">Finance Frontier 2024 <br />
+                        Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        This event brought financial literacy to the forefront with simulations, mock stock trading, and entrepreneurial challenges. Finance Frontier 2024 was designed to inspire budding economists and business leaders of tomorrow.
+                    </p>
+                </div>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Roborumble 2023 <br /> Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                    Roborumble 2023 featured intense battles between student-built robots. Teams competed in both strategy and engineering skill, pushing the limits of automation, design, and teamwork in an electrifying environment.
-                </p>
-            
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+                <div className="content">
+                    <h3 className="about-heading">Roborumble 2023 <br /> Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        Roborumble 2023 featured intense battles between student-built robots. Teams competed in both strategy and engineering skill, pushing the limits of automation, design, and teamwork in an electrifying environment.
+                    </p>
+                </div>
+
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">LitLantern 2024 <br /> Year: 2023–24 | Category: Cultural</h3>
-                <p className="about-text">
-                    LitLantern 2024 was a festival of words, celebrating literature, poetry, and storytelling. The event illuminated campus with the spark of imagination, featuring open mics, debates, and creative writing workshops.
-                </p>
+                <div className="content">
+                    <h3 className="about-heading">LitLantern 2024 <br /> Year: 2023–24 | Category: Cultural</h3>
+                    <p className="about-text">
+                        LitLantern 2024 was a festival of words, celebrating literature, poetry, and storytelling. The event illuminated campus with the spark of imagination, featuring open mics, debates, and creative writing workshops.
+                    </p>
+                </div>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Sportfiesta 2025 <br /> Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                    A grand celebration of sportsmanship and competitive spirit, Sportfiesta 2025 brought together athletes from all disciplines. With thrilling matches and vibrant cheering, it became a memorable highlight of the academic year.
-                </p>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+
+                <div className="content">
+                    <h3 className="about-heading">Sportfiesta 2025 <br /> Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        A grand celebration of sportsmanship and competitive spirit, Sportfiesta 2025 brought together athletes from all disciplines. With thrilling matches and vibrant cheering, it became a memorable highlight of the academic year.
+                    </p>
+                </div>
             </div>
 
             <div className="about-block">
-                <h3 className="about-heading">Pulse Night 2024 <br /> Year: 2023–24 | Category: Sports</h3>
-                <p className="about-text">
-                    Pulse Night 2024 was a high-energy music and dance extravaganza. Featuring live performances, DJ sets, and vibrant lights, the event captured the heartbeat of youth and became an unforgettable party night for students..
-                </p>
+                <div className="content">
+                    <h3 className="about-heading">Pulse Night 2024 <br /> Year: 2023–24 | Category: Sports</h3>
+                    <p className="about-text">
+                        Pulse Night 2024 was a high-energy music and dance extravaganza. Featuring live performances, DJ sets, and vibrant lights, the event captured the heartbeat of youth and became an unforgettable party night for students..
+                    </p>
+                </div>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
             </div>
 
             <div className="about-block">
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+
+                <div className="content">
                 <h3 className="about-heading">Stagestorm 2023 <br /> Year: 2023–24 | Category: Sports</h3>
                 <p className="about-text">
                     Stagestorm 2023 brought drama, comedy, and creativity center stage. This theatrical event was a platform for aspiring performers, actors, and directors to showcase their talent in front of an enthusiastic audience.
                 </p>
+                </div>
             </div>
 
             <div className="about-block">
+                
+                <div className="content">
                 <h3 className="about-heading">Bioverse 2022 <br /> Year: 2023–24 | Category: Sports</h3>
                 <p className="about-text">
                     Bioverse 2022 took students on a journey through the wonders of biology and environmental science. With exhibitions, quizzes, and interactive experiments, it sparked curiosity about the living world.
                 </p>
+                </div>
+
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
             </div>
 
             <div className="about-block">
+                <div className="banner">
+                    <img src="./campushighlights.jpg" alt="" />
+                </div>
+
+                <div className="content">
                 <h3 className="about-heading">Canvas Carnival 2024 <br /> Year: 2023–24 | Category: Sports</h3>
                 <p className="about-text">
                     Canvas Carnival 2024 was a riot of colors and creativity. Students displayed their artistic talents through painting, digital art, and installations, turning the campus into a vibrant gallery of youthful expression.
                 </p>
+                </div>
             </div>
 
         </div>
